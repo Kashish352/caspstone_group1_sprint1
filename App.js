f9e9ed41fe4abf51891b20fb838d3e5ec0bd1914
Kashish352 <kashish2k201@gmail.com>
2024-10-08 17:41:45 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme } from "./utils/Themes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import { useState } from "react";
+import Authentication from "./pages/Authentication";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+
+function App() {
+  const [openAuth, setOpenAuth] = useState(false);
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+          <Navbar setOpenAuth={setOpenAuth} />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/home" exact element={<Home />} />
+            <Route path="/contact" exact element={<Contact />} />
+          </Routes>
+          {openAuth && (
+            <Authentication openAuth={openAuth} setOpenAuth={setOpenAuth} />
+          )}
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ Navbar.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import styled from "styled-components";
+import LogoImg from "../utils/Images/Logo.png";
+import { NavLink } from "react-router-dom";
+import Button from "./Button";
+import {
+  FavoriteBorder,
+  MenuRounded,
+  SearchRounded,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
+
+const Nav = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  color: white;
+`;
+const NavbarContainer = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  padding: 0 24px;
+  display: flex;
+  gap: 14px;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
+`;
+const NavLogo = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 0 6px;
+  font-weight: 500;
+  font-size: 18px;
+  text-decoration: none;
+  color: inherit;
+`;
+const Logo = styled.img`
+  height: 150px;
+`;
+const NavItems = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 0 6px;
+  list-style: none;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+const Navlink = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 1s slide-in;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+  &.active {
+    color: ${({ theme }) => theme.primary};
+    border-bottom: 1.8px solid ${({ theme }) => theme.primary};
+  }
+`;
+const ButtonContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 28px;
+  align-items: center;
+  padding: 0 6px;
+  color: ${({ theme }) => theme.primary};
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Navbar = ({ openAuth, setOpenAuth, currentUser }) => {
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo>
+          <Logo src={LogoImg} />
+        </NavLogo>
+        <NavItems>
+          <Navlink to="/">Home</Navlink>
+          <Navlink to="/Shop">Shop</Navlink>
+          <Navlink to="/New_Arrivals">New Arrivals</Navlink>
+          <Navlink to="/Orders">Orders</Navlink>
+          <Navlink to="/Contact">Contact</Navlink>
+        </NavItems>
+        <Navlink to="/search">
+          <SearchRounded sx={{ color: "inherit", fontSize: "30px" }} />
+        </Navlink>
+        <Button text="SignIn" small onClick={() => setOpenAuth(!openAuth)} />
+      </NavbarContainer>
+    </Nav>
+  );
+};
+
+export default Navbar;
